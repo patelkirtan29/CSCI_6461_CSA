c88b46ef2c0943e7feb9e2cf3b3064ed6fce3f95
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2025-11-27 19:00:55 -0500

    Fix Program 2: main code starts at LOC 100, data moved to higher addresses for correct IPL execution

--- resources/program2.asm
+++ resources/program2.asm
@@ -1,7 +1,7 @@
 # Program2.asm - Paragraph Search Program (CSCI 6461 ISA)
 # Reads a paragraph from memory, prints it, asks for a word, searches for it, and prints result
 
-LOC 100
+LOC 144
 # Paragraph data (ASCII codes for 6 sentences, terminated by 0)
 Data 72   ; 'H'
 Data 101  ; 'e'
@@ -169,101 +169,97 @@ Data 100  ; 'd'
 Data 0
 
 # CODE SECTION
-LOC 300
+LOC 100
 ; Print paragraph
-LDR 0,0,100         ; R0 = paragraph pointer
-PRINT_LOOP LDR 1,0,0 ; R1 = [R0]
-JZ 1,PRINT_END      ; If char == 0, end
+LDR 0,0,144         ; R0 = paragraph pointer
+LDR 1,0,0           ; R1 = [R0]
+JZ 1,312            ; If char == 0, end (PRINT_END)
 OUT 1,1             ; Print char to printer
 AIR 0,1             ; R0++
-JUMP PRINT_LOOP
-PRINT_END
-; Print prompt
+JZ 0,301            ; Unconditional jump to PRINT_LOOP
 LDR 2,0,230         ; R2 = prompt pointer
-PROMPT_LOOP LDR 3,0,0 ; R3 = [R2]
-JZ 3,PROMPT_END     ; If char == 0, end
+LDR 3,0,0           ; R3 = [R2]
+JZ 3,318            ; If char == 0, end (PROMPT_END)
 OUT 3,1             ; Print char
 AIR 2,1             ; R2++
-JUMP PROMPT_LOOP
-PROMPT_END
-; Read search word
+JZ 0,314            ; Unconditional jump to PROMPT_LOOP
 LDR 4,0,200         ; R4 = search word buffer pointer
-READ_LOOP IN 5,0    ; Read char from keyboard
-JZ 5,READ_END       ; If char == 0, end
+	IN 0,0              ; Read char from keyboard (use R0)
+JZ 5,325            ; If char == 0, end (READ_END)
 SIR 5,10            ; Check for newline (ASCII 10)
-JZ 5,READ_END       ; If newline, end
-STR 5,0,0           ; Store char in buffer
+JZ 5,325            ; If newline, end (READ_END)
+	STR 0,0,0           ; Store char in buffer
 AIR 4,1             ; R4++
-JUMP READ_LOOP
-READ_END STR 6,0,0  ; Store 0 terminator
-; Search paragraph for word
-LDR 0,0,100         ; R0 = paragraph pointer
+JZ 0,320            ; Unconditional jump to READ_LOOP
+	STR 1,0,0           ; Store 0 terminator (READ_END)
+LDR 0,0,144         ; R0 = paragraph pointer
 LDR 7,0,220         ; R7 = sentenceIndex
 LDR 6,0,221         ; R6 = wordIndex
-SEARCH_LOOP LDR 1,0,0 ; R1 = [R0]
-JZ 1,NOT_FOUND      ; If char == 0, end
+LDR 1,0,0           ; R1 = [R0]
+JZ 1,340            ; If char == 0, end (NOT_FOUND)
 SIR 1,46            ; '.'
-JZ 1,NEXT_SENTENCE
+JZ 1,335            ; NEXT_SENTENCE
 SIR 1,33            ; '!'
-JZ 1,NEXT_SENTENCE
+JZ 1,335            ; NEXT_SENTENCE
 SIR 1,63            ; '?'
-JZ 1,NEXT_SENTENCE
+JZ 1,335            ; NEXT_SENTENCE
 SIR 1,32            ; ' '
-JZ 1,NEXT_WORD
+JZ 1,332            ; NEXT_WORD
 SIR 1,44            ; ','
-JZ 1,NEXT_WORD
+JZ 1,332            ; NEXT_WORD
 AIR 6,1             ; wordIndex++
 LDR 4,0,200         ; R4 = search word buffer
 LDR 5,0,0           ; R5 = paragraph word pointer
-WORD_MATCH_LOOP LDR 2,0,0 ; R2 = search word char
+LDR 2,0,0           ; R2 = search word char
 LDR 3,0,0           ; R3 = paragraph char
-JZ 2,WORD_MATCH_END ; If search char == 0, end
-JZ 3,WORD_MATCH_FAIL; If paragraph char == 0, fail
-SIR 2,3             ; Compare chars
-JNZ 2,WORD_MATCH_FAIL
+JZ 2,352            ; If search char == 0, end (WORD_MATCH_END)
+JZ 3,357            ; If paragraph char == 0, fail (WORD_MATCH_FAIL)
+	SIR 2,3             ; Compare chars
+	JZ 2,347            ; If equal, continue WORD_MATCH_LOOP
+	JZ 0,357            ; If not equal, unconditional jump to WORD_MATCH_FAIL
 AIR 4,1             ; Next search char
 AIR 5,1             ; Next paragraph char
-JUMP WORD_MATCH_LOOP
-WORD_MATCH_END LDR 3,0,0 ; Next paragraph char
+JZ 0,347            ; Unconditional jump to WORD_MATCH_LOOP
+LDR 3,0,0           ; Next paragraph char (WORD_MATCH_END)
 SIR 3,32            ; ' '
-JZ 3,FOUND
+JZ 3,359            ; FOUND
 SIR 3,44            ; ','
-JZ 3,FOUND
+JZ 3,359            ; FOUND
 SIR 3,46            ; '.'
-JZ 3,FOUND
+JZ 3,359            ; FOUND
 SIR 3,33            ; '!'
-JZ 3,FOUND
+JZ 3,359            ; FOUND
 SIR 3,63            ; '?'
-JZ 3,FOUND
-JUMP WORD_MATCH_FAIL
-FOUND LDR 2,0,250    ; Found message
-FOUND_MSG_LOOP LDR 3,0,0 ; R3 = [R2]
-JZ 3,FOUND_MSG_END   ; If char == 0, end
-OUT 3,1              ; Print char
-AIR 2,1              ; R2++
-JUMP FOUND_MSG_LOOP
-FOUND_MSG_END LDR 4,0,200 ; Print word
-PRINT_WORD_LOOP LDR 3,0,0 ; R3 = [R4]
-JZ 3,PRINT_WORD_END  ; If char == 0, end
-OUT 3,1              ; Print char
-AIR 4,1              ; R4++
-JUMP PRINT_WORD_LOOP
-PRINT_WORD_END LDR 3,0,220 ; sentenceIndex
+JZ 3,359            ; FOUND
+JZ 0,357            ; Unconditional jump to WORD_MATCH_FAIL
+LDR 2,0,250         ; Found message (FOUND)
+LDR 3,0,0           ; R3 = [R2]
+JZ 3,364            ; If char == 0, end (FOUND_MSG_END)
+OUT 3,1             ; Print char
+AIR 2,1             ; R2++
+JZ 0,361            ; Unconditional jump to FOUND_MSG_LOOP
+LDR 4,0,200         ; Print word (FOUND_MSG_END)
+LDR 3,0,0           ; R3 = [R4]
+JZ 3,368            ; If char == 0, end (PRINT_WORD_END)
+OUT 3,1             ; Print char
+AIR 4,1             ; R4++
+JZ 0,366            ; Unconditional jump to PRINT_WORD_LOOP
+LDR 3,0,220         ; sentenceIndex (PRINT_WORD_END)
 OUT 3,1
 LDR 3,0,221         ; wordIndex
 OUT 3,1
 HLT
-WORD_MATCH_FAIL JUMP NEXT_WORD
-NEXT_WORD AIR 0,1    ; R0++
-JUMP SEARCH_LOOP
-NEXT_SENTENCE AIR 7,1 ; sentenceIndex++
-LDA 6,0,0            ; wordIndex = 0
-AIR 0,1              ; R0++
-JUMP SEARCH_LOOP
-NOT_FOUND LDR 2,0,260 ; Not found message
-NOTFOUND_MSG_LOOP LDR 3,0,0 ; R3 = [R2]
-JZ 3,NOTFOUND_MSG_END; If char == 0, end
-OUT 3,1              ; Print char
-AIR 2,1              ; R2++
-JUMP NOTFOUND_MSG_LOOP
-NOTFOUND_MSG_END HLT
+JZ 0,332            ; Unconditional jump to WORD_MATCH_FAIL (address 357)
+AIR 0,1             ; R0++ (NEXT_WORD)
+JZ 0,326            ; Unconditional jump to SEARCH_LOOP (address 326)
+AIR 7,1             ; sentenceIndex++ (NEXT_SENTENCE)
+LDA 6,0,0           ; wordIndex = 0
+AIR 0,1             ; R0++
+JZ 0,326            ; Unconditional jump to SEARCH_LOOP (address 326)
+LDR 2,0,260         ; Not found message (NOT_FOUND)
+LDR 3,0,0           ; R3 = [R2]
+JZ 3,377            ; If char == 0, end (NOTFOUND_MSG_END)
+OUT 3,1             ; Print char
+AIR 2,1             ; R2++
+JZ 0,375            ; Unconditional jump to NOTFOUND_MSG_LOOP
+HLT                 ; NOTFOUND_MSG_END
